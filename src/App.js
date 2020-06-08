3bd35d5226e21180d7e3896565f9aa681c403f36
Max Hayes <hayesmax21@gmail.com>
2020-06-08 19:59:06 -0400

    updated code to use classes and display number of playlists using fake data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // 
-function App() {
-  return (
-    <div className="App">
-        <h1>lyricify</h1>
-    </div>
-  );
+let fakeServerData = {
+  user: {
+    name: "Max",
+    playlists: [
+      {
+        name: "playlist 1",
+        songs: ["song 1", "song 2", "song 3"],
+
+      },
+      {
+        name: "playlist 2",
+        songs: ["song 4", "song 5", "song 6"]
+      }
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
 }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.serverData.user ?
+        <div>
+          <h1>lyricify</h1>
+          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+        </div> : <h1>Loading...</h1>
+        }
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
