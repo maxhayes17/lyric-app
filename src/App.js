ea09ee19df2b3df4e542a8a7c0b705ad5093a8bc
Max Hayes <hayesmax21@gmail.com>
2020-06-07 23:17:35 -0400

    Changed background color, fixed class so h1 aligned at top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// 
 function App() {
   return (
-    <div className="App-header">
+    <div className="App">
         <h1>lyricify</h1>
     </div>
   );
