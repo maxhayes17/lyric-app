aac59132531eb1e6c6cef6030fb7bfc54755534f
Max Hayes <hayesmax21@gmail.com>
2020-06-12 21:42:43 -0400

    LOTS of additions
    -dynamically uses spotify playlists/songs
    -displays pictures for playlists
    -can filter for playlists or songs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 // 
-let fakeServerData = {
-  user: {
-    name: "Max",
-    playlists: [
-      {
-        name: "playlist 1",
-        songs: ["song 1", "song 2", "song 3"],
 
-      },
-      {
-        name: "plablist 2",
-        songs: ["song 4", "song 5", "song 6"]
-      }
-    ]
-  }
-};
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist
+    return (
+        <div>
+          <input type="image" src={playlist.imageUrl} className="image"/>
+          <h3>{this.props.playlist.name}</h3>
+        </div>
+    );
+  }
+}
+class Songs extends Component {
+  render() {
+
     return (
       <div>
-        <img></img>
-        <h3>{this.props.playlist.name}</h3>
+          <h1 className="plName">{this.props.playlist.name}</h1>
+          <ul>
+            {this.props.playlist.songs.map(song =>
+              <li className="songList">{song.name}</li>
+              )}
+          </ul>
       </div>
     );
   }
@@ -62,30 +64,95 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({serverData: fakeServerData});
-    }, 1000);
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed.access_token
+    if (!accessToken) {
+      return;
+    }
+    else {
+          fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.display_name
+      }
+    }))
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(playlistData => {
+      let playlists = playlistData.items
+      let trackDataPromises = playlists.map(playlist => {
+        let responsePromise = fetch(playlist.tracks.href, {
+          headers: {'Authorization': 'Bearer ' + accessToken}
+        })
+        let trackDataPromise = responsePromise
+        .then(response => response.json())
+        return trackDataPromise
+      })
+      let allTracksDataPromises = 
+      Promise.all(trackDataPromises)
+      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+        trackDatas.forEach((trackData, i) => {
+          playlists[i].trackDatas = trackData.items
+          .map(item => item.track)
+          .map(trackData => ({
+            name: trackData.name,
+            duration: trackData.duration_ms / 1000
+          }))
+      })
+      return playlists
+    })
+    return playlistsPromise
+  })
+    .then(playlists => this.setState({
+      playlists: playlists.map(item => {
+        return {
+          name: item.name,
+          imageUrl: item.images[0].url, 
+          songs: item.trackDatas
+          
+        }
+    })
+    }))
+
+
+    }
+
   }
   render() {
-    let playlistToRender = this.state.serverData.user ? 
-    this.state.serverData.user.playlists.filter(playlist =>
-      playlist.name.toLowerCase().includes(
+    let playlistToRender = this.state.user && this.state.playlists ? 
+    this.state.playlists.filter(playlist => {
+      let matchesPlaylist = playlist.name.toLowerCase().includes(
         this.state.filterString.toLowerCase())
-    ) : []
+      let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+      .includes(this.state.filterString.toLowerCase()))
+        return matchesPlaylist || matchesSong
+      }) 
+      : []
     return (
       <div className="App">
-        {this.state.serverData.user ? // ? = if true, run code, if else, run whatever's after :
-        <div>
-          <h1>lyricify</h1>
-
+        {this.state.user ? // ? = if true, run code, if else, run whatever's after :
+        <div> 
+          <h1 className="title">lyricify</h1>
           <Filter onTextChange={text => this.setState({filterString: text})}/>
-
           <PlaylistCounter playlists={playlistToRender}/>
 
-          {playlistToRender.map(playlists => 
-    
-          <Playlist playlist={playlists}/>)} 
-        </div> : <h1>Loading...</h1>
+          {playlistToRender.map(playlists =>
+          <div className="ply">
+          <Playlist playlist={playlists}/>
+          </div>)}
+          {playlistToRender.map(playlists =>
+          <div className="songsUI">
+            <Songs playlist={playlists}/>
+          </div>)}
+          
+
+ 
+
+        </div> : <div className="center"><button onClick={()=>window.location="http://localhost:8888/login"}
+        className="but">Sign in with Spotify</button></div>
         }
       </div>
     );
